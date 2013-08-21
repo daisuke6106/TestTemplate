943a5cbd733295dd9861d58f165a8ca0ebaef6c4
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-08-21 14:02:56 +0000

    static要素にfinalが定義されていなかったため追加
    git-svn-id: file:///opt/svn/repository/projects/trunk@213 425babaa-d5dd-46a5-afc5-8c713db7da9f

--- src/jp/co/dk/test/template/property/TestTemplateProperty.java
+++ src/jp/co/dk/test/template/property/TestTemplateProperty.java
@@ -8,28 +8,28 @@ import jp.co.dk.property.exception.PropertyException;
 public class TestTemplateProperty extends AbstractProperty{
 	
 	/** コンソール表示用ヘッダー */
-	public static TestTemplateProperty CONSOLE_HEADER = new TestTemplateProperty("console.header");
+	public final static TestTemplateProperty CONSOLE_HEADER = new TestTemplateProperty("console.header");
 	
 	/** コンソール表示用フッター */
-	public static TestTemplateProperty CONSOLE_FOTTER = new TestTemplateProperty("console.fotter");
+	public final static TestTemplateProperty CONSOLE_FOTTER = new TestTemplateProperty("console.fotter");
 	
 	/** テスト用・一時作業ディレクトリ作成 */
-	public static TestTemplateProperty CREATE_TEST_TEMP_DIR = new TestTemplateProperty("create.test.tempdir");
+	public final static TestTemplateProperty CREATE_TEST_TEMP_DIR = new TestTemplateProperty("create.test.tempdir");
 	
 	/** テスト用・一時作業ディレクトリ削除 */
-	public static TestTemplateProperty DELETE_TEST_TEMP_FILE = new TestTemplateProperty("delete.test.tempdir");
+	public final static TestTemplateProperty DELETE_TEST_TEMP_FILE = new TestTemplateProperty("delete.test.tempdir");
 	
 	/** テスト用・一時作業ディレクトリ */
-	public static TestTemplateProperty TEST_TEMP_DIR = new TestTemplateProperty("test.tempdir");
+	public final static TestTemplateProperty TEST_TEMP_DIR = new TestTemplateProperty("test.tempdir");
 	
 	/** テスト用・一時作業ファイル */
-	public static TestTemplateProperty TEST_TEMP_FILE = new TestTemplateProperty("test.tempfile");
+	public final static TestTemplateProperty TEST_TEMP_FILE = new TestTemplateProperty("test.tempfile");
 	
 	/** テスト用・存在しないディレクトリ */
-	public static TestTemplateProperty TEST_FRAUD_TEMP_DIR = new TestTemplateProperty("test.fraud.tempdir");
+	public final static TestTemplateProperty TEST_FRAUD_TEMP_DIR = new TestTemplateProperty("test.fraud.tempdir");
 	
 	/** テスト用・存在しないファイル */
-	public static TestTemplateProperty TEST_FRAUD_TEMP_FILE = new TestTemplateProperty("test.fraud.tempfile");
+	public final static TestTemplateProperty TEST_FRAUD_TEMP_FILE = new TestTemplateProperty("test.fraud.tempfile");
 	
 	protected TestTemplateProperty(String key) throws PropertyException {
 		super(new File("properties/test/TestTemplate.properties"), key);
